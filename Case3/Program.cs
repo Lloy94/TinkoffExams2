2a8a04ca00ed1772e69a3f9d554b34e783c3a0b5
Илья Тимофеев <Mcdos14@ya.ru>
2022-12-13 02:26:13 +0500

    первое решение 3 задачи

--- Case3/Program.cs
+++ Case3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Case3
 {
@@ -6,7 +8,32 @@ namespace Case3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+            int a =int.Parse (Console.ReadLine());
+            var dict = new Dictionary<int,string>();
+            int m=0;
+            int n=0;
+            for (int i = 1; i <= a; i++)
+            {
+                m = i;
+                n = a - i;
+                for (int j = 1; j < (n * m + 1); j++)
+                {
+                    if (j % m == 0 && j % n == 0)
+                    {
+                        int nok = j;
+                            string s = $"{m} {n}";
+                           if(!dict.ContainsKey(nok)) dict.Add(nok, s);
+                    }
+                }
+               
+            }
+            var dict1 = dict.OrderBy(p => p.Key);
+            string result = dict1.First().Value;
+            string[] results = result.Split(' ');
+            int result1 = int.Parse(results[0]);
+            int result2 = int.Parse(results[1]);
+            Console.WriteLine($"{result1}  {result2}");
         }
     }
 }
