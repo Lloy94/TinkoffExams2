80a679c093962a12d2faac8dfef45263c56367ab
Илья Тимофеев <Mcdos14@ya.ru>
2022-12-13 00:16:15 +0500

    неполное решение

--- TinkoffExams2/Program.cs
+++ TinkoffExams2/Program.cs
@@ -6,7 +6,23 @@ namespace TinkoffExams2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int count = int.Parse( Console.In.ReadLine());
+            string facility = Console.In.ReadLine();
+            string paint = Console.In.ReadLine();
+            string []text = facility.Split(' ');
+            int uglyCount = 0;
+            int textCount = -1;
+            foreach (string s in text) {
+                textCount += s.Length+1;
+                for (int i = textCount-s.Length+1; i < textCount-1; i++)
+                {
+                    if (paint[i] == paint[i - 1]) {
+                        uglyCount++;
+                        break;
+                            };
+                }
+                    }
+            Console.Out.WriteLine(uglyCount);
         }
     }
 }
